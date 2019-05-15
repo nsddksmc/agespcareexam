31db56c3edeca86d93a49d8ecee1fa184ce12ea3
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2019-05-15 13:41:45 +0300

    agespcareexam

---
+++ story_content/user.js
@@ -0,0 +1,116 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ZhMOnp7KsI":
+        Script1();
+        break;
+      case "6pwhXYewWUG":
+        Script2();
+        break;
+      case "5gtjU98n1Qt":
+        Script3();
+        break;
+      case "6HqWvRZ8s2X":
+        Script4();
+        break;
+      case "5uUbSE7p5uB":
+        Script5();
+        break;
+      case "5wWjy4hLLYc":
+        Script6();
+        break;
+      case "5WBlWPyJ5Fg":
+        Script7();
+        break;
+      case "5faMhGOEPOn":
+        Script8();
+        break;
+      case "6YNQwtZRvNd":
+        Script9();
+        break;
+      case "60Cu6yswwjN":
+        Script10();
+        break;
+      case "5htSqegVoGT":
+        Script11();
+        break;
+      case "5x9y2x2c9P6":
+        Script12();
+        break;
+      case "67Wy0YclWSe":
+        Script13();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script6()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script7()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script8()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script9()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script10()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script11()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script12()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script13()
+{
+  $("#tab-customlink").hide();
+}
+
